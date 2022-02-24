fbd308ce570ca9559625a50c4c76510fc111c8d0
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-24 15:09:04 +0530

    js for video modal in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,7 @@ let play_button = document.querySelector('.play-button'),
 play_video = document.querySelector('.play-video');
 
 play_button.addEventListener('click',()=>{
-    alert();
-    play_video.style.tranform="scale(1)";
+    play_video.style.display = "block";
 })
 
 
