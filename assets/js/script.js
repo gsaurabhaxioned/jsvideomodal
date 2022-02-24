5bd7d5b2c9b2dc45af9bc577818c1a811e606929
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-24 15:04:00 +0530

    js code started for video modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,10 @@
-/* Author: 
+let play_button = document.querySelector('.play-button'),
+play_video = document.querySelector('.play-video');
 
-*/
-
-
-
-
-
-
-
-
+play_button.addEventListener('click',()=>{
+    alert();
+    play_video.style.tranform="scale(1)";
+})
 
 
 
