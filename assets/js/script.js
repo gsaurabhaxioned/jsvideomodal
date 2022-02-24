eff045349292af9d7f8b7e64d0391b091729b103
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-24 17:07:34 +0530

    js added for hamburger

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
+
+$(document).ready(function(){
+    $(".hamburger").click(function(){
+         $(".menus").slideToggle();
+        $(this).toggleClass("cross");
+    });
+});
+
+
 let play_button = document.querySelector('.play-button'),
 play_video = document.querySelector('.play-video'),
 close_video = document.querySelector('.close-video');
+
 
 play_button.addEventListener('click',()=>{
     play_video.style.display = "block";
@@ -11,9 +21,6 @@ close_video.addEventListener('click',()=>{
     play_video.style.display = "none";
     close_video.style.display = "none";
 })
-
-
-
 
 
 
